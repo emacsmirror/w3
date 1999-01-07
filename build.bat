816c1731103a6d9d75f98bce31547dfe9975ea9c
William M. Perry <wmperry@gnu.org>
1999-01-07 15:25:41 +0000

    *** empty log message ***

--- build.bat
+++ build.bat
@@ -1,8 +1,21 @@
-set EMACSDIR=c:\emacs
+@echo off
 
-set EMACS=%EMACS%\bin\emacs.exe
-set WIDGETDIR=%EMACS%\lisp
-set GNUSDIR=%EMACS%\lisp
+set FALLBACKEMACSDIR=c:\emacs
+
+REM This allows the user to specify the emacs root on the command
+REM line.
+set COMMANDLINEGIVEN=yes
+if "%1" == "" set COMMANDLINEGIVEN=no
+if "%COMMANDLINEGIVEN%" == "yes" set EMACSDIR=%1
+
+REM If no emacs directory is set in the default environment, use our
+REM fallback.
+if "%EMACSDIR%" == "" echo EMACSDIR not set!  Using %FALLBACKEMACSDIR% as a default...
+if "%EMACSDIR%" == "" set EMACSDIR=%FALLBACKEMACSDIR%
+
+set EMACS=%EMACSDIR%\bin\emacs.exe
+set WIDGETDIR=%EMACSDIR%\lisp
+set GNUSDIR=%EMACSDIR%\lisp
 
 cd lisp
 copy w3-cfg.nt w3-cfg.el
